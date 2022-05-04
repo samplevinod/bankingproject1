e7e44dc5210dceac88f8624846e1a4028c6be50c
samplevinod <104816596+samplevinod@users.noreply.github.com>
2022-05-04 11:13:28 +0530

    Add files via upload

---
+++ com.revature/src/main/java/com/revature/entity/AccDetails.java
@@ -0,0 +1,88 @@
+package com.revature.entity;
+
+public class AccDetails {
+
+
+	private int id;
+	private String user_name;
+	private String phone_no;
+	private String address;
+	private double balance;
+	private String bank_name;
+
+	public AccDetails(String user_name, String phone_no, String address, double opening_balance,String bank_name) {
+		super();
+	//	this.id = id;
+		this.user_name = user_name;
+		this.phone_no = phone_no;
+		this.address = address;
+		this.balance = opening_balance;
+		this.bank_name = bank_name;
+	}
+	
+	public AccDetails() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getUser_name() {
+		return user_name;
+	}
+
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+
+	public String getPhone_no() {
+		return phone_no;
+	}
+
+
+	public void setPhone_no(String phone_no) {
+		this.phone_no = phone_no;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public double getBalance() {
+		return balance;
+	}
+
+
+	public void setBalance(double balance) {
+		this.balance= balance;
+	}
+	public String getbank_name() {
+		return bank_name;
+	}
+
+
+	public void setbank_name(String bank_name) {
+		this.bank_name = bank_name;
+	}
+	@Override
+	public String toString() {
+		return "AccDetails [id :" + id + ", user_name : " + user_name + ", phone_no : " + phone_no +", address : "+address+", balance : " +balance+",bank_name : " + bank_name +"]";
+}
+
+}
